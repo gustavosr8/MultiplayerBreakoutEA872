0141a2b11668a8e1a083728bdc92f151318ec26e
gustavosr8 <gustavosr8@gmail.com>
2021-09-29 14:10:27 -0300

    Backup versao demo

--- bin/CMakeFiles/bolinha.dir/cmake_clean.cmake
+++ bin/CMakeFiles/bolinha.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/bolinha.dir/src/model/bolinha.cpp.o"
-  "libbolinha.a"
   "libbolinha.pdb"
+  "libbolinha.a"
 )
 
 # Per-language clean rules from dependency scanning.
